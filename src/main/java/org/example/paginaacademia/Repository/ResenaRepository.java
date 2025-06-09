0b2b3324ea8488667e384b635e59d6f46635db0a
Sebastian <sebastianllatasotoya15@gmail.com>
2025-06-08 21:46:06 -0500

    Nuevo formulario de reseña, con su respectivo controlador, repositorio para la conexión a base de datos 7u7

---
+++ src/main/java/org/example/paginaacademia/Repository/ResenaRepository.java
@@ -0,0 +1,8 @@
+package org.example.paginaacademia.Repository;
+
+import org.example.paginaacademia.Model.Resena;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResenaRepository extends JpaRepository<Resena, Long> {
+
+}
